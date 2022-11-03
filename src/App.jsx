57e7f3c5abe5a6881a0ea7cf67f5ffceebebe83d
Deiber Olivares <olivaresdei6@gmail.com>
2022-11-02 23:14:11 -0500

    Se creo un método para buscar las imagenes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    /* Realizar una pagina web que muestre por pagina 30 imagenes de pixabay*/
+
+    const registrosPorPagina = 30;
+    const [paginaActual, setPaginaActual] = useState(1);
+    const [imagenes, setImagenes] = useState([]);
+    const [totalPaginas, setTotalPaginas] = useState(1);
+    const [busqueda, setBusqueda] = useState('');
+
+    const consultarAPI = async () => {
+        const key = '27831392-ee0593f708bc31745e094e459';
+        const url = `https://pixabay.com/api/?key=${key}&q=$perro&per_page=${registrosPorPagina}&page=${paginaActual}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        console.log(resultado);
+        setImagenes(resultado.hits);
+
+    }
+
+    useEffect(() => {
+        consultarAPI();
+    }, []);
 
     return (
         <div>
